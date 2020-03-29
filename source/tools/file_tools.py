b19bdad188e1e453872cf854ae1c257d595639fe
gongming <791099982@qq.com>
2020-03-29 17:39:02 -0400

    Initial commit

---
+++ source/tools/file_tools.py
@@ -0,0 +1,59 @@
+import json
+
+import pandas as pd
+import os
+import struct
+import numpy as np
+
+
+# get cwd
+def get_cwd():
+    return os.getcwd()
+
+
+# join path
+def join_path(*args):
+    path = ''
+    for v in args:
+        path = os.path.join(path, v)
+    return path
+
+
+# root path.  os.path.pardir = ..
+# basic_path = join_path(get_cwd(), os.path.pardir, 'data')
+basic_path = join_path(get_cwd(), 'resource')
+
+
+# judge whether file exist
+def is_exist(*args):
+    full_path = join_path(basic_path, *args)
+    flag = os.path.exists(full_path)
+    return flag
+
+
+# tans Dataform to nparray
+def df2np(df):
+    return np.array(df)
+
+
+# load csv
+def load_csv(path):
+    data = pd.read_csv(path)
+    return data
+
+
+# save csv
+def save_csv(data, path):
+    df = pd.DataFrame(data)
+    df.to_csv(path, index=False, sep=',')
+
+
+# load json
+def load_json(*args):
+    with open(join_path(basic_path, *args), 'r') as load_f:
+        load_dict = json.load(load_f)
+        return load_dict
+
+
+
+
